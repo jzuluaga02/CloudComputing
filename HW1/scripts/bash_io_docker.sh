1d6db903aaceb2bd49b4ce41b39ea8b23bee71a3
Juan Zuluaga <jzuluaga@scu.edu>
2022-10-14 15:41:01 -0700

    version actualizada del script de io

--- HW1/scripts/bash_io_docker.sh
+++ HW1/scripts/bash_io_docker.sh
@@ -3,10 +3,12 @@
 for i in {1..5}
 do
   sysbench --test=fileio --file-test-mode=seqwr run >> output2.txt
-  sysbench --test=fileio --file-test-mode=seqwr --file-num=5 --file-block-size=64
-  sysbench --test=fileio --file_test-mode=rndwr --file-num=10 --file-block-size=128 --file-io-mode=async
-  sysbench --test=fileio --file_test-mode=seqrd --file-num=40 --file-block-size=512 --file-test-mode=sync
-  sysbench --test=fileio --file-test-mode=seqrewr --file-num=45 --file-block-size=1024 --file-io-mode=async
+  sysbench --test=fileio --file-test-mode=seqwr --file-num=5 --file-block-size=64 prepare
+  sysbench --test=fileio --file-test-mode=seqwr --file-num=5 --file-block-size=64 run >> output2.txt
+  sysbench --test=fileio --file_test-mode=rndwr --file-num=10 --file-block-size=128 --file-io-mode=async prepare
+  sysbench --test=fileio --file_test-mode=rndwr --file-num=10 --file-block-size=128 --file-io-mode=async run >> output2.txt
+  sysbench --test=fileio --file-test-mode=seqrewr --file-num=45 --file-block-size=1024 --file-io-mode=async prepare
+  sysbench --test=fileio --file-test-mode=seqrewr --file-num=45 --file-block-size=1024 --file-io-mode=async run >> output2.txt
 done
 
 
