bd0d8c2937ad8aa5c403e0559318d3ad1acdbf9e
Juan Zuluaga <jzuluaga@scu.edu>
2022-10-14 16:16:59 -0700

    version actualizada del script de cpu

--- HW1/scripts/bash_cpu_docker.sh
+++ HW1/scripts/bash_cpu_docker.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-for i in {1000..20000..5000}
+for j in {1..5}
 do
-   printf "Test with  cpu max prime of $i\n" >> output.txt
-   sysbench --test=cpu  --cpu-max-prime=$i run >> output.txt
+ for i in {1000..20000..5000}
+  do
+    printf "Test with  cpu max prime of $i\n" >> output.txt
+    sysbench --test=cpu  --cpu-max-prime=$i run >> output.txt
+  done
 done
-
 
